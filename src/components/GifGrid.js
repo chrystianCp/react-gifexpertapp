b3ee52200729d430afab37754c67585ac751410c
DeathApex <chrystian.cardenas@outlook.com>
2021-12-05 19:33:43 -0600

    commit curso cap85

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( {category} ) => {
 
-    const [images, setImages] = useState([]);
+    
+    const { data:images, loading } = useFetchGifs( category );
 
-    useEffect( () => {
-        getGifs( category )
-            .then( setImages );
-    }, [ category ])
-   
 
     return (
         <>
-        <h3>{category}</h3>   
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>   
+            
+            { loading && <p className="animate__animated animate__flash">Loading</p> }
+
             <div className="card-grid">
                 
                     {
